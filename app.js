12769251e61252650d8968fc7dc51bb187c28b7e
Aaron Strick <aaronstrick@gmail.com>
2015-01-13 16:57:04 -0800

    Serve static files in pub folder, add stylesheet

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(express.static(__dirname + '/public'));
+
 app.use('/', router)
 
 app.listen(3000);
